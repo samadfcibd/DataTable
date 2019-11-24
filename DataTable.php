f0705733d6da9490002724127ec5e11a3c2b5f2a
Abdus Samad <samad.ezzetech1@gmail.com>
2019-11-24 23:58:42 +0600

    Update

--- DataTable.php
+++ DataTable.php
@@ -5,9 +5,59 @@ namespace DT_library;
 class DataTable
 {
 
-    public static function toTable($array)
+    /*
+     * 1 = Single array
+     * 2 = Multidimensional array
+     */
+    private $input_type;
+
+    private $keys;
+
+    protected static $key_count;
+
+    public function toTable($array)
     {
-        $keys = array_keys($array);
-        print_r($keys);
+        $this->keys = self::get_array_keys($array);
+
+        $table_head_columns = '<thead>';
+        $table_body_columns = '<tbody>';
+
+        if ($this->input_type == 1)
+        {
+            foreach ($array as $key => $value)
+            {
+                $table_head_columns .= '<th>' . $key . '</th>';
+                $table_body_columns .= '<tr><td>' . $value . '</td></tr>';
+            }
+        } else {
+            foreach ($this->keys as $key) {
+                $table_head_columns .= '<th>' . $key . '</th>';
+
+                $column_values = array_column($array, $key);
+                $table_body_columns .= '<tr>';
+                foreach ($column_values as $value) {
+                    $table_body_columns .= '<td>' . $value . '</td>';
+                }
+                $table_body_columns .= '</tr>';
+            }
+        }
+        $table_head_columns .= '</thead>';
+        $table_body_columns .= '</tbody>';
+        return '<table class="table table-bordered">' . $table_head_columns . $table_body_columns . '</table>';
+    }
+
+    private function get_array_keys($array)
+    {
+        if (count($array) != count($array, COUNT_RECURSIVE)) {
+            $this->input_type = 2;
+            $key_temp = [];
+            foreach ($array as $key => $value) {
+                foreach ($value as $key => $value) {
+                    array_push($key_temp, $key);
+                }
+            }
+            return array_unique($key_temp);
+        }
+        $this->input_type = 1;
     }
 }
--- index.php
+++ index.php
@@ -1,21 +1,64 @@
 <?php
 require('DataTable.php');
+
 use DT_library\DataTable;
 
+//$data = [
+//    [
+//        'name' => 'Abdus Samad',
+//        'email' => 'samadocpl@gmail.com'
+//    ],
+//    [
+//        'name' => 'Ibrahim Ahad',
+//        'email' => 'ahad@gmail.com'
+//    ]
+//];
 $data = [
-    [
         'name' => 'Abdus Samad',
         'email' => 'samadocpl@gmail.com'
-    ],
-    [
-        'name' => 'Ibrahim Ahad',
-        'email' => 'ahad@gmail.com'
-    ]
 ];
 
 
 $table = new DataTable();
+$html_table = $table->toTable($data);
+//var_dump($html_table);
+?>
 
-echo $table::toTable($data);
+
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container" id="main-content">
+    <h2>Welcome to my website!</h2>
+    <p>Some content goes here! Let's go with the classic "lorem ipsum."</p>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+    </p>
+
+    <?php echo $html_table; ?>
+</div>
+
+<script
+        src="https://code.jquery.com/jquery-3.4.1.slim.js"
+        integrity="sha256-BTlTdQO9/fascB1drekrDVkaKd9PkwBymMlHOiG+qLI="
+        crossorigin="anonymous"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
+
+</body>
+</html>
 
 
